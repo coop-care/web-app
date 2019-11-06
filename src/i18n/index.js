c88f6e81224c0448d2ceb2bedd795702f3687fa0
Michael Kamphausen <mail@michael-kamphausen.de>
2019-11-06 18:25:15 +0100

    added glossary

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,11 +1,16 @@
 import Terminology from "../helper/terminology";
-import enUS from "./en-us";
-import deDE from "./de-de";
 
+import enUS from "./en-us";
 import terminologyEN from "./en-us/terminology.json";
+import glossaryEN from "./en-us/glossary.json";
 enUS.terminology = Terminology.makeIds(terminologyEN);
+enUS.glossary = glossaryEN;
+
+import deDE from "./de-de";
 import terminologyDE from "./de-de/terminology.json";
+import glossaryDE from "./de-de/glossary.json";
 deDE.terminology = Terminology.makeIds(terminologyDE);
+deDE.glossary = glossaryDE;
 
 export default {
   "en-us": enUS,
