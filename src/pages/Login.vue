d0cb40aa178cf8fbe848ad4d4d6fead7ea7f9d32
Michael Kamphausen <mail@michael-kamphausen.de>
2019-12-20 17:22:40 +0100

    bugfix for problem summary cards with fixed width, adjusted login and registration cards for mobile layout and consistency

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,30 +1,47 @@
 <template>
-  <q-page class="window-height window-width row justify-center items-center">
+  <q-page class="window-height window-width row justify-center">
     <div class="column">
-      <div v-if="errorMsg" class="row">
-        <p class="text-red q-my-md">Error: {{ errorMsg }}</p>
-      </div>
       <div class="row">
-        <h5 class="text-h5 q-my-md">{{ $t('welcome') }}!</h5>
-      </div>
-      <div class="row">
-        <q-card square bordered class="bg-grey-4 q-pa-lg shadow-1">
+        <q-card class="credentials bg-grey-2 shadow-1 q-mt-xl">
           <q-card-section>
+            <div class="text-h5 text-center">{{ $t("welcome") }}!</div>
+          </q-card-section>
+          <q-card-section>
+            <p v-if="errorMsg" class="text-red q-mt-md">{{ errorMsg }}</p>
             <q-form class="q-gutter-md">
-              <q-input square filled clearable v-model="email" type="email" :label="$t('email')" />
-              <q-input square filled clearable v-model="password" type="password" :label="$t('password')" />
+              <q-input
+                clearable
+                v-model="email"
+                type="email"
+                :label="$t('email')"
+              />
+              <q-input
+                clearable
+                v-model="password"
+                type="password"
+                :label="$t('password')"
+              />
             </q-form>
           </q-card-section>
           <q-card-actions class="q-px-md">
-            <q-btn unelevated color="primary" size="lg" class="full-width"
-              :label="$t('login')" @click="doLogin"
+            <q-btn
+              unelevated
+              color="primary"
+              class="full-width"
+              :label="$t('login')"
+              @click="doLogin"
+            />
+            <q-btn
+              flat
+              dense
+              no-caps
+              size="md"
+              color="primary"
+              class="text-center full-width q-mt-sm"
+              :label="$t('notRegistered') + ' ' + $t('createAccount')"
+              :to="{ name: 'register' }"
             />
           </q-card-actions>
-          <q-card-section class="text-center q-pa-none">
-            <router-link :to="{ name: 'register' }">
-              {{ $t('notRegistered') }} {{ $t('createAccount') }}
-            </router-link>
-          </q-card-section>
         </q-card>
       </div>
     </div>
@@ -32,36 +49,34 @@
 </template>
 
 <style lang="sass">
-.q-card 
-  width: 360px
+.q-card.credentials
+  width: 300px
 </style>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import {
-  StitchUser,
-  UserPasswordCredential,
-} from 'mongodb-stitch-browser-sdk';
+import Vue from "vue";
+import Component from "vue-class-component";
+import { StitchUser, UserPasswordCredential } from "mongodb-stitch-browser-sdk";
 
 @Component
 export default class PageLogin extends Vue {
-  email = '';
-  password = '';
+  email = "";
+  password = "";
 
-  errorMsg = '';
+  errorMsg = "";
 
   doLogin() {
     // console.log('logging in...', this.email, this.password);
     const credential = new UserPasswordCredential(this.email, this.password);
     this.$stitchApi.stitch.auth
       .loginWithCredential(credential)
-      .then((user) => {
+      .then(user => {
         // console.log(`Logged in as user with id: ${user.id}`);
-        this.$router.push({ name: 'index' });
+        this.$router.push({ name: "index" });
       })
-      .catch(err => { this.errorMsg = err.message });
-    
+      .catch(err => {
+        this.errorMsg = err.message;
+      });
   }
 }
 </script>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,20 +1,34 @@
 <template>
-  <q-page class="window-height window-width row justify-center items-center">
+  <q-page class="window-height window-width row justify-center">
     <div v-if="isForm" class="column">
       <div class="row">
-        <h5 class="text-h5 q-my-md">{{ $t('createAccount') }}</h5>
-      </div>
-      <div class="row">
-        <q-card square bordered class="bg-grey-4 q-pa-lg shadow-1">
+        <q-card class="credentials bg-grey-2 shadow-1 q-mt-xl">
+          <q-card-section>
+            <div class="text-h5 text-center">{{ $t("createAccount") }}</div>
+          </q-card-section>
           <q-card-section>
             <q-form class="q-gutter-md">
-              <q-input square filled clearable v-model="email" type="email" :label="$t('email')" />
-              <q-input square filled clearable v-model="password" type="password" :label="$t('password')" />
+              <q-input
+                clearable
+                v-model="email"
+                type="email"
+                :label="$t('email')"
+              />
+              <q-input
+                clearable
+                v-model="password"
+                type="password"
+                :label="$t('password')"
+              />
             </q-form>
           </q-card-section>
-          <q-card-actions class="q-px-md">
-            <q-btn unelevated color="primary" size="lg" class="full-width"
-              :label="$t('create')" @click="doRegister"
+          <q-card-actions class="q-px-md q-pb-md">
+            <q-btn
+              unelevated
+              color="primary"
+              class="full-width"
+              :label="$t('create')"
+              @click="doRegister"
             />
           </q-card-actions>
         </q-card>
@@ -23,12 +37,12 @@
     <div v-else class="column">
       <div v-if="isSuccess" class="row">
         <h5 class="text-h5 q-my-md">
-          {{ $t('confirmationMessage') }}
+          {{ $t("confirmationMessage") }}
         </h5>
       </div>
       <div v-else class="row">
         <h5 class="text-h5 q-my-md">
-          An error has occured:<br>
+          An error has occured:<br />
           {{ errorMsg }}
         </h5>
       </div>
@@ -37,36 +51,47 @@
 </template>
 
 <style lang="sass">
-.q-card 
-  width: 360px
+.q-card.credentials
+  width: 300px
 </style>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import {
-  UserPasswordAuthProviderClient,
-} from 'mongodb-stitch-browser-sdk';
+import Vue from "vue";
+import Component from "vue-class-component";
+import { UserPasswordAuthProviderClient } from "mongodb-stitch-browser-sdk";
 
-enum State { Form, Success, Error }
+enum State {
+  Form,
+  Success,
+  Error
+}
 
 @Component
 export default class PageLogin extends Vue {
   state = State.Form;
-  errorMsg = '';
+  errorMsg = "";
 
-  email = '';
-  password = '';
+  email = "";
+  password = "";
 
-  get isForm() { return this.state === State.Form }
-  get isSuccess() { return this.state === State.Success }
+  get isForm() {
+    return this.state === State.Form;
+  }
+  get isSuccess() {
+    return this.state === State.Success;
+  }
 
   doRegister() {
-    const epclient = this.$stitchApi.stitch.auth
-      .getProviderClient(UserPasswordAuthProviderClient.factory);
-    epclient.registerWithEmail(this.email, this.password)
-      .then(() => this.state = State.Success)
-      .catch(err => { this.state = State.Error; this.errorMsg = err.message });
+    const epclient = this.$stitchApi.stitch.auth.getProviderClient(
+      UserPasswordAuthProviderClient.factory
+    );
+    epclient
+      .registerWithEmail(this.email, this.password)
+      .then(() => (this.state = State.Success))
+      .catch(err => {
+        this.state = State.Error;
+        this.errorMsg = err.message;
+      });
   }
 }
 </script>
