83440feb8ecb4337a95cd2e3b08747554a8bf9de
Michael Kamphausen <mail@michael-kamphausen.de>
2020-01-03 19:11:01 +0100

    removed glossary because it is not needed in a generic way

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,14 +1,10 @@
 import enUS from "./en-us";
 import terminologyEN from "./en-us/terminology.json";
-import glossaryEN from "./en-us/glossary.json";
 enUS.terminology = terminologyEN;
-enUS.glossary = glossaryEN;
 
 import deDE from "./de-de";
 import terminologyDE from "./de-de/terminology.json";
-import glossaryDE from "./de-de/glossary.json";
 deDE.terminology = terminologyDE;
-deDE.glossary = glossaryDE;
 
 import { register } from "timeago.js";
 import { de as timeagoDE } from "timeago.js/lib/lang";
