841cdf0745d0c7984b0e7df523a33d257732f4fa
Michael Kamphausen <mail@michael-kamphausen.de>
2021-01-29 01:13:07 +0100

    new components: SplitView, SelectableInput, NoDataItem, LabeledItem and LabeledValueEditor

---
+++ src/components/LabeledValueEditor.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="labeled-value-item"> 
+    <div
+      v-for="(item, index) in items"
+      :key="keyPrefix + index"
+      class="row"
+    >
+      <div class="col-5 row items-center">
+        <q-btn
+          icon="remove"
+          color="negative"
+          :title="$t('delete')"
+          unelevated
+          round
+          dense
+          size="9.5px"
+          @click="$emit('remove', index)"
+        />
+        <selectable-input
+          :value="item.label"
+          :options="labels"
+          @input="$emit('input:label', {target: item, value: $event})"
+          dense
+          class="unlabeled-selectable-input col q-pl-md q-pr-xs no-text-size-adjust"
+        />
+      </div>
+      <div class="col-7 q-pl-xs">
+        <slot v-bind:item="item" />
+      </div>
+    </div>
+    <div
+      class="row items-center q-py-sm cursor-pointer non-selectable contact-add-button can-hover text-primary"
+      @click="$emit('add')"
+    >
+      <q-btn
+        icon="add"
+        :title="addButtonLabel"
+        color="positive"
+        unelevated
+        round
+        dense
+        size="9.5px"
+      />
+      <div class="q-pl-md text-body2 text-weight-medium no-text-size-adjust">
+        {{ addButtonLabel }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+.labeled-value-item > *
+  border-bottom: 1px solid #cccccc
+.unlabeled-selectable-input.q-field--auto-height.q-field--dense.q-field--labeled .q-field__control-container
+  padding-top: 2px
+.contact-add-button
+  &.can-hover:hover
+    position: relative
+    &::before
+      content: ''
+      position: absolute
+      width: 100%
+      height: 100%
+      top: 0
+      left: 0
+      z-index: -1
+      background-color: currentColor
+      opacity: .1
+</style>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { LabeledValue } from "../models";
+import SelectableInput from "../components/SelectableInput.vue";
+
+@Component({
+  components: {
+    SelectableInput
+  }
+})
+export default class ContactView extends Vue {
+  @Prop({ type: Array, default: () => [] }) readonly items!: LabeledValue<any>[];
+  @Prop({ type: Array, default: () => [] }) readonly labels!: LabeledValue<string>[];
+  @Prop({ type: String, default: "" }) readonly addButtonLabel!: string;
+  @Prop({ type: String, default: "" + Math.random() }) readonly keyPrefix!: string;
+}
+</script>
---
+++ src/components/SelectableInput.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-select
+    :value="value"
+    :label="label"
+    :options="filteredOptions"
+    map-options
+    emit-value
+    :dense="dense"
+    options-dense
+    use-input
+    hide-selected
+    fill-input
+    input-debounce="0"
+    :clearable="clearable"
+    @input="$emit('input', $event || '')"
+    @input-value="inputValue = $event;"
+    @keydown.enter.tab="selectInputValue"
+    @keydown.tab="lastTabKeyDownTimestamp = Date.now()"
+    @blur="onBlur"
+    @filter="filter"
+    @popup-show="select.reset()"
+    ref="select"
+  />
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Ref } from "vue-property-decorator";
+import { QSelect } from "quasar";
+import { LabeledValue } from "../models";
+
+@Component
+export default class SelectableInput extends Vue {
+  @Prop({ type: String, default: "" }) readonly value!: string;
+  @Prop({ type: String, default: "" }) readonly label!: string;
+  @Prop({ type: Array, default: () => [] }) readonly options!: LabeledValue<string>[];
+  @Prop(Boolean) readonly dense!: boolean;
+  @Prop(Boolean) readonly clearable!: boolean;
+  @Ref() readonly select!: QSelect;
+
+  filteredOptions: LabeledValue<string>[] = this.options;
+  lastTabKeyDownTimestamp = 0;
+  inputValue = "";
+
+  onBlur() {
+    if (this.lastTabKeyDownTimestamp + 100 < Date.now()) {
+      this.selectInputValue();
+    }
+  }
+  selectInputValue() {
+    const value = this.inputValue.trim();
+    const needle = value.toLocaleLowerCase();
+    const existingOption = this.options.find(option => 
+      option.label.toLocaleLowerCase() == needle || option.value == value
+    );
+
+    if (!existingOption) {
+      this.filteredOptions = this.options;
+      this.$emit("input", value);
+      this.$emit("new-value", value);
+    } else if (existingOption.value != this.value) {
+      this.filteredOptions = this.options;
+      this.$emit("input", existingOption.value);
+    }
+  }
+  filter(value: string, update: (_: () => void) => void) {
+    update(() => {
+      const needle = value.toLocaleLowerCase()
+      this.filteredOptions = this.options.filter(item => 
+        item.label.toLocaleLowerCase().indexOf(needle) > -1
+      );
+    })
+  }
+}
+</script>
