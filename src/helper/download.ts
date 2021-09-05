224ea6ae3087b94825b25e7c8b9a9855f6ff8b6f
Michael Kamphausen <mail@michael-kamphausen.de>
2021-09-05 14:14:59 +0200

    team settings: added button to download backup of client and team data in internal json format

--- src/helper/download.ts
+++ src/helper/download.ts
@@ -5,20 +5,9 @@ export function download(filename: string, dataUri: string) {
     linkElement.click();
 }
 
-export function downloadJSON(data: any, filename: string) {
-    const json = JSON.stringify(
-        data,
-        (key, value) => {
-            if (key == "_id") {
-                return Array.prototype.map.call(new Uint8Array(value.id), x => ("00" + x.toString(16)).slice(-2)).join("");
-            } else {
-                return value;
-            }
-        },
-        2
-    );
-    const jsonUri =
-        "data:application/json;charset=utf-8," + encodeURIComponent(json);
+export function downloadJSON(data: any, filename: string, pretty = false) {
+    const json = JSON.stringify(data, undefined, pretty ? 2 : undefined);
+    const jsonUri = "data:application/json;charset=utf-8," + encodeURIComponent(json);
     const exportFileDefaultName = encodeURIComponent(filename);
     download(exportFileDefaultName, jsonUri);
 }
