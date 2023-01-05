d7f341fa1c56f5a67e144ac8922e0f69c69f09aa
Michael Kamphausen <mail@michael-kamphausen.de>
2023-01-05 14:45:22 +0100

    bugfixes: close navigation drawer with a tap instead of a swipe, and use the full height to swipe between tabs

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -1,69 +1,83 @@
 <template>
   <q-drawer
     v-model="isVisible"
-    content-class="bg-grey-2 dense-avatar"
+    content-class="no-scroll non-selectable bg-grey-2 dense-avatar"
     show-if-above
-    class="print-hide"
+    no-swipe-close
+    :breakpoint="1020"
+    class="navigation-drawer print-hide"
   >
-    <pull-to-refresh>
-      <q-tabs
-        v-model="selectedTab"
-        no-caps
-        class="navigation-drawer-tabs  border-bottom-grey q-mt-sm q-mb-md text-grey-8"
-        dense
-        align="justify"
-        active-color="primary"
-      >
-        <q-tab
-          name="clients"
-          :ripple="false"
-          :label="$tc('client', 2)"
-          icon="fas fa-house-user"
-        />
-        <q-tab
-          name="team"
-          :ripple="false"
-          :label="$t('team')"
-          icon="fas fa-users"
-        />
-        <q-tab
-          name="user"
-          :ripple="false"
-          :label="$t('myApp')"
-          icon="fas fa-user-circle"
-        />
-      </q-tabs>
-      <q-tab-panels
-        v-model="selectedTab"
-        animated
-        swipeable
-        class="bg-transparent"
-      >
-        <q-tab-panel
-          name="clients"
-          class="q-pa-none"
+    <pull-to-refresh class="fit">
+      <div class="fit column no-wrap">
+        <q-tabs
+          v-model="selectedTab"
+          no-caps
+          class="border-bottom-grey q-mt-sm text-grey-8"
+          dense
+          align="justify"
+          active-color="primary"
         >
-          <client-drawer/>
-        </q-tab-panel>
-        <q-tab-panel
-          name="team"
-          class="q-pa-none"
+          <q-tab
+            name="clients"
+            :ripple="false"
+            :label="$tc('client', 2)"
+            icon="fas fa-house-user"
+          />
+          <q-tab
+            name="team"
+            :ripple="false"
+            :label="$t('team')"
+            icon="fas fa-users"
+          />
+          <q-tab
+            name="user"
+            :ripple="false"
+            :label="$t('myApp')"
+            icon="fas fa-user-circle"
+          />
+        </q-tabs>
+        <div
+          style="height:16px"
+          @click="closeDrawer"
+        ></div>
+        <q-tab-panels
+          v-model="selectedTab"
+          animated
+          swipeable
+          class="col scroll bg-transparent"
         >
-          <team-drawer/>
-        </q-tab-panel>
-        <q-tab-panel
-          name="user"
-          class="q-pa-none"
-        >
-          <my-account-drawer/>
-        </q-tab-panel>
-      </q-tab-panels>
+          <q-tab-panel
+            name="clients"
+            class="q-pa-none column no-wrap"
+          >
+            <client-drawer/>
+            <q-space @click="closeDrawer"/>
+          </q-tab-panel>
+          <q-tab-panel
+            name="team"
+            class="q-pa-none column no-wrap"
+          >
+            <team-drawer/>
+            <q-space @click="closeDrawer"/>
+          </q-tab-panel>
+          <q-tab-panel
+            name="user"
+            class="q-pa-none column no-wrap"
+          >
+            <my-account-drawer/>
+            <q-space @click="closeDrawer"/>
+          </q-tab-panel>
+        </q-tab-panels>
+      </div>
     </pull-to-refresh>
   </q-drawer>
 </template>
 
 <style lang="sass">
-.navigation-drawer-tabs
+.navigation-drawer
+  .q-pull-to-refresh__content
+    height: 100%
+    width: 100%
   .q-tab
     flex: 10000 1 0%
     padding: 0 4px
